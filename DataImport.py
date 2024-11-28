747d16470040486bce110cb5ae036b88589e484b
EimanteZemyteVU <eimante.zemyte@mif.stud.vu.lt>
2024-11-28 22:20:47 +0200

    Add more visuals to expl.opynb file

--- DataImport.py
+++ DataImport.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
 
-# Define directory
-new_directory = r"C:\Users\Eimante\OneDrive - Vilnius University\Documents\GitHub\Taxi"
-# Change the current working directory
-os.chdir(new_directory)
+# # Define directory
+# new_directory = r"C:\Users\Eimante\OneDrive - Vilnius University\Documents\GitHub\Taxi"
+# # Change the current working directory
+# os.chdir(new_directory)
+
+# Get the directory of the script itself
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Read the CSV file into a DataFrame
 trips = pd.read_csv("yellow_tripdata_2019-01_full.csv")
